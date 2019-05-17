deab1a501ea68fcb2c635fac904b530c524ae4fc
aaronsato <2asato@gmail.com>
2019-05-17 10:57:22 -0700

    edit articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,8 @@
 class ArticlesController < ApplicationController
     def new
       @article = Article.new
-     end
-     def create
+    end
+    def create
         @article = Article.new(article_params)
         if @article.save
         #  flash[:notice] = "Article was successfully created"
@@ -10,13 +10,29 @@ class ArticlesController < ApplicationController
         else
          render 'new'
         end
-       end
-
-       def show
-        @article = Article.find(params[:id])
-       end
-    private
-      def article_params
-       params.require(:article).permit(:title, :description)
-      end
     end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+         flash[:notice] = "Article was updated"
+         redirect_to article_path(@article)
+        else
+         flash[:notice] = "Article was not updated"
+         render 'edit'
+        end
+       end
+    
+    private
+    def article_params
+    params.require(:article).permit(:title, :description)
+    end
+end
